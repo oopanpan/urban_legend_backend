792c4f7d244c32c1bd1ccaa8c444b6fc0af134f8
oopanpan <oopanpan@gmail.com>
2021-04-07 20:56:05 -0700

    added more association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
+  has_many :likes,
+  has_many :liked_user, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :posts
     has_many :comments
     has_many :commented_posts, through: :comments, source: :post
+    has_many :likes
+    has_many :liked_posts, through: :likes, source: :post
 end
