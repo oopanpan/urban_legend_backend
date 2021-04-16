e4809a57f3b782371d8359e20d7fd7c6e73787fd
oopanpan <oopanpan@gmail.com>
2021-04-15 19:17:58 -0700

    before refractoring for new rendering for post and comment

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,6 +2,7 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :header, :content, :keyword, :user
   has_one :user
   has_many :comments
+  has_many :likes
 
   def user
     {id:object.user.id, username: object.user.username}
