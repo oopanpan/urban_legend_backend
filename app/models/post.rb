ed74b0842e862c8ac3e74e9513fe2bb9685749be
oopanpan <oopanpan@gmail.com>
2021-04-15 19:54:48 -0700

    post dependency on destroy fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, as: :commentable
-  has_many :likes
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :linked_users, through: :likes, source: :users
 end
