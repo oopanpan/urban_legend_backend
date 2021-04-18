5366717b4daee0f7f81dd5ce1831f89ccd73ccf5
oopanpan <oopanpan@gmail.com>
2021-04-18 06:59:47 -0700

    friendship relationship stable

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::FriendshipssController < ApplicationController 
 
     def create
-
+        friendship = Friendship.new
+        friendship.user_id = params[:user_id]
+        friendship.friend_id = params[:friend_id]
+        if friendship.save
+            render json: {message: 'sucess'}
+        else
+            render json: {message: 'fail'}
+        end
     end
 
     def destroy
-        
+        friendship = Friendship.find(params[:id])
+        friendship.destroy
+        render json: {message: 'success'}
     end
 end
