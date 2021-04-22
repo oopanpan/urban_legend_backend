5ce2abd7b3213dcfb2c2268fbfb12959ef722459
oopanpan <oopanpan@gmail.com>
2021-04-22 12:09:39 -0700

    added default bio

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,6 +28,7 @@ class Api::V1::UsersController < ApplicationController
             content_type: 'application/png'
         )
         user.keyword='Global'
+        user.bio="I'm a cosmopolitan."
         if user.save
             token = encode_token({ user_id: user.id })
             render json: { user: UserSerializer.new(user), jwt: token }, status: :created
