fb40b7ff96823897d6394ccca9913d36d69edb9e
oopanpan <oopanpan@gmail.com>
2021-05-06 07:28:56 -0700

    deleted comments and unused file

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -7,13 +7,6 @@ class CommentSerializer < ActiveModel::Serializer
 
   def user
     {id:object.user.id, username: object.user.username, avatar: Rails.application.routes.url_helpers.rails_blob_path(object.user.avatar, only_path: true)}
-    # ActiveModel::SerializableResource.new(object.user,  serializer: UserSerializer)
   end
 
-  # def comment
-  #   object.comments.each do |comment|
-  #     {content: comment.content, user:{id: comment.user.id, username: comment.user.username}}
-  #   end
-  #   ActiveModel::SerializableResource.new(object.comments,  each_serializer: CommentSerializer)
-  # end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email_address, :bio, :keyword, :liked_posts, :avatar, :friendships, :inverse_friendships
   has_many :posts
-  # has_many :friends
-  # has_many :inverse_friends
-
   
-  # private
   def avatar
     Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
   end
