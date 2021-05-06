b6e04be9e1b3f9d577058183abb0cf01394dd16a
oopanpan <oopanpan@gmail.com>
2021-05-06 14:19:33 -0700

    deleting a thing

--- app/serializers/friendship_serializer.rb
+++ app/serializers/friendship_serializer.rb
@@ -1,4 +1,4 @@
-class FriendshipsSerializer < ActiveModel::Serializer
+class FriendshipSerializer < ActiveModel::Serializer
     attributes :id, :user, :friend
     belongs_to :user
     belongs_to :friend, class_name: 'User'
